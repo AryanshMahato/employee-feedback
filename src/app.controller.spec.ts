f1685988236e16de50631ff9c44197e8902320df
aryanshmahato <aryanshmahato@gmail.com>
2021-01-03 22:59:46 +0530

    [EF-1] (Add) health controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,26 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { InternalServerErrorException } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EnvConfig } from './config/EnvConfig';
+import {
+  HealthIndicatorResult,
+  MongooseHealthIndicator,
+  TerminusModule,
+} from '@nestjs/terminus';
 
-describe('AppController', () => {
+describe('Health', () => {
   let appController: AppController;
+  let mongooseHealthIndicator: MongooseHealthIndicator;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(EnvConfig.databaseURL, {
+          useNewUrlParser: true,
+        }),
+        TerminusModule,
+      ],
       controllers: [AppController],
       providers: [],
     }).compile();
 
+    mongooseHealthIndicator = await app.resolve<MongooseHealthIndicator>(
+      MongooseHealthIndicator,
+    );
     appController = app.get<AppController>(AppController);
   });
 
-  describe('when health is called with no mongo connection', () => {
-    it('should internal server error exception', () => {
-      try {
-        appController.getHealth();
-      } catch (e) {
-        expect(e).toBeInstanceOf(InternalServerErrorException);
-      }
+  // full testing will be done in e2e testing
+  describe('when health is called with mongo connection', () => {
+    it('should return status ok', async () => {
+      jest.spyOn(mongooseHealthIndicator, 'pingCheck').mockImplementation(
+        async (): Promise<HealthIndicatorResult> => ({
+          mongodb: {
+            status: 'up',
+          },
+        }),
+      );
+
+      const healthCheckResult = await appController.getHealth();
+      expect(healthCheckResult.status).toBe('ok');
     });
   });
 });
