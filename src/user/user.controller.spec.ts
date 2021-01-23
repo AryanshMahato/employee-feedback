80ed58a005edb01334670dc1c36de6c4521ccc4a
aryanshmahato <aryanshmahato@gmail.com>
2021-01-23 13:05:01 +0530

    [EF-5] (Add) unit test for UserController.signUp()

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,16 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { getModelToken } from '@nestjs/mongoose';
 import { User } from './user.schema';
-import { forwardRef } from '@nestjs/common';
+import { ConflictException, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { EnvConfig } from '../config/EnvConfig';
 import { UserService } from './user.service';
 import { UserModelMock } from './user.mock';
 import { TeamModuleMock } from '../team/team.mock';
 import { AuthModuleMock } from '../auth/auth.mock';
+import { SignUpRequestBody } from './user.validation';
+import { AuthService } from '../auth/auth.service';
+import { SignUpResponse } from './user.types';
+import { MongoError } from 'mongodb';
 
 describe('UsersController', () => {
   let controller: UserController;
+  let service: UserService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,9 +37,133 @@ describe('UsersController', () => {
     }).compile();
 
     controller = await module.get<UserController>(UserController);
+    service = await module.get<UserService>(UserService);
+    authService = await module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('signUp()', () => {
+    describe('When signup is called and user is created successfully', () => {
+      const Mock = {
+        id: 'userId',
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+      };
+
+      it('should return mock userId', async () => {
+        const signUpMock = jest
+          .spyOn(service, 'signUp')
+          .mockImplementation(async () => {
+            return Mock.id;
+          });
+
+        const generateAccessTokenMock = jest
+          .spyOn(authService, 'generateAccessToken')
+          .mockImplementation(async () => {
+            return Mock.accessToken;
+          });
+
+        const generateRefreshTokenMock = jest
+          .spyOn(authService, 'generateRefreshToken')
+          .mockImplementation(async () => {
+            return Mock.refreshToken;
+          });
+
+        const userId = await controller.signUp({} as SignUpRequestBody);
+
+        expect(userId).toEqual(Mock);
+
+        expect(signUpMock).toBeCalledTimes(1);
+        expect(generateAccessTokenMock).toBeCalledTimes(1);
+        expect(generateRefreshTokenMock).toBeCalledTimes(1);
+      });
+    });
+
+    describe('When signup is called and user already exists in database', () => {
+      const Data = {
+        id: 'userId',
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+      };
+
+      it('should return mock userId', async () => {
+        const signUpMock = jest
+          .spyOn(service, 'signUp')
+          .mockImplementation(async () => {
+            throw new MongoError({ code: 11000 });
+          });
+
+        const generateAccessTokenMock = jest
+          .spyOn(authService, 'generateAccessToken')
+          .mockImplementation(async () => {
+            return Data.accessToken;
+          });
+
+        const generateRefreshTokenMock = jest
+          .spyOn(authService, 'generateRefreshToken')
+          .mockImplementation(async () => {
+            return Data.refreshToken;
+          });
+
+        let response: SignUpResponse;
+
+        try {
+          response = await controller.signUp({} as SignUpRequestBody);
+        } catch (e) {
+          expect(response).not.toBeDefined();
+          expect(e).toBeInstanceOf(ConflictException);
+          expect(e?.message).toBe('Conflict in user signup');
+
+          expect(signUpMock).toBeCalledTimes(1);
+          expect(generateAccessTokenMock).toBeCalledTimes(0);
+          expect(generateRefreshTokenMock).toBeCalledTimes(0);
+        }
+      });
+    });
+
+    describe('When signup is called and there is some unknown error', () => {
+      const Data = {
+        id: 'userId',
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+      };
+
+      it('should return mock userId', async () => {
+        const signUpMock = jest
+          .spyOn(service, 'signUp')
+          .mockImplementation(async () => {
+            throw new Error('unknown error');
+          });
+
+        const generateAccessTokenMock = jest
+          .spyOn(authService, 'generateAccessToken')
+          .mockImplementation(async () => {
+            return Data.accessToken;
+          });
+
+        const generateRefreshTokenMock = jest
+          .spyOn(authService, 'generateRefreshToken')
+          .mockImplementation(async () => {
+            return Data.refreshToken;
+          });
+
+        let response: SignUpResponse;
+
+        try {
+          response = await controller.signUp({} as SignUpRequestBody);
+        } catch (e) {
+          expect(response).not.toBeDefined();
+          expect(e).toBeInstanceOf(Error);
+          expect(e?.message).toBe('unknown error');
+
+          expect(signUpMock).toBeCalledTimes(1);
+          expect(generateAccessTokenMock).toBeCalledTimes(0);
+          expect(generateRefreshTokenMock).toBeCalledTimes(0);
+        }
+      });
+    });
   });
 });
