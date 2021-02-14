771514e5952b525bbe8f31489b5dd39cbac18b93
Aryansh Mahato <aryanshmahato@gmail.com>
2021-02-14 21:38:51 +0530

    [EF-5] (Add) unit test for GetUserValidation query

--- src/user/user.validation.spec.ts
+++ src/user/user.validation.spec.ts
@@ -1,4 +1,7 @@
-import { GetUserValidationParams } from './user.validation';
+import {
+  GetUserValidationParams,
+  GetUserValidationQuery,
+} from './user.validation';
 import { validateOrReject, ValidationError } from 'class-validator';
 
 describe('GetUserValidationParams', () => {
@@ -16,9 +19,70 @@ describe('GetUserValidationParams', () => {
     it('should throw error', async () => {
       try {
         const getUserValidationParams = new GetUserValidationParams();
-        // getUserValidationParams.userId = 'testId';
 
         await validateOrReject(getUserValidationParams);
+        expect('There should be an error').toBeFalsy();
+      } catch (e) {
+        expect(e).toBeInstanceOf(Array);
+
+        if (Array.isArray(e)) {
+          e.forEach((e) => {
+            expect(e).toBeInstanceOf(ValidationError);
+          });
+        }
+      }
+    });
+  });
+});
+
+describe('GetUserValidationQuery', () => {
+  describe('When valid methods are passed', () => {
+    it('should not throw any error', async () => {
+      const getUserValidationQuery = new GetUserValidationQuery();
+      getUserValidationQuery.method = 'username';
+
+      await validateOrReject(getUserValidationQuery);
+      expect.anything();
+    });
+
+    it('should not throw any error', async () => {
+      const getUserValidationQuery = new GetUserValidationQuery();
+      getUserValidationQuery.method = 'email';
+
+      await validateOrReject(getUserValidationQuery);
+      expect.anything();
+    });
+  });
+
+  describe('When invalid method is passed', () => {
+    it('should throw validation error', async () => {
+      try {
+        const getUserValidationQuery = new GetUserValidationQuery();
+        // @ts-ignore
+        getUserValidationQuery.method = 'invalid';
+
+        await validateOrReject(getUserValidationQuery);
+        expect('There should be an error').toBeFalsy();
+      } catch (e) {
+        expect(e).toBeInstanceOf(Array);
+
+        if (Array.isArray(e)) {
+          e.forEach((e) => {
+            expect(e).toBeInstanceOf(ValidationError);
+          });
+        }
+      }
+    });
+  });
+
+  describe('When empty string is passed as method', () => {
+    it('should throw validation error', async () => {
+      try {
+        const getUserValidationQuery = new GetUserValidationQuery();
+        // @ts-ignore
+        getUserValidationQuery.method = '';
+
+        await validateOrReject(getUserValidationQuery);
         expect('There should be an error').toBeFalsy();
       } catch (e) {
         expect(e).toBeInstanceOf(Array);
