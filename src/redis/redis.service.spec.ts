c7d06bf25386ad8463ce8df4c883e3039d302d2b
Aryansh Mahato <aryanshmahato@gmail.com>
2021-02-21 00:12:30 +0530

    [EF-5] (Add) unit test for RedisService.health()

--- src/redis/redis.service.spec.ts
+++ src/redis/redis.service.spec.ts
@@ -15,4 +15,24 @@ describe('RedisService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('health()', () => {
+    describe('When database is connected', () => {
+      it('should return OK as health', () => {
+        // @ts-ignore
+        service.redis = { status: 'OK' };
+
+        expect(service.health()).toBe('OK');
+      });
+    });
+
+    describe('When database is not connected', () => {
+      it('should return OK as health', () => {
+        // @ts-ignore
+        service.redis = { status: 'Down' };
+
+        expect(service.health()).toBe('Down');
+      });
+    });
+  });
 });
