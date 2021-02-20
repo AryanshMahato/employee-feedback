e02c556c9ec647dbead6cc77511c970c0ea86502
Aryansh Mahato <aryanshmahato@gmail.com>
2021-02-21 00:22:35 +0530

    [EF-5] (Add) unit test for RedisService.isRefreshTokenValid()

---
+++ src/redis/redis.mock.ts
@@ -0,0 +1,5 @@
+export class MockRedis {
+  lrange(): void {
+    return;
+  }
+}
--- src/redis/redis.service.spec.ts
+++ src/redis/redis.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisService } from './redis.service';
+import { MockRedis } from './redis.mock';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('RedisService', () => {
   let service: RedisService;
@@ -32,6 +34,38 @@ describe('RedisService', () => {
         service.redis = { status: 'Down' };
 
         expect(service.health()).toBe('Down');
+      });
+    });
+  });
+
+  describe('isRefreshTokenValid()', () => {
+    describe('When refresh token is present in db', () => {
+      it('should not throw any error', async () => {
+        const mockRedis = new MockRedis();
+        // @ts-ignore
+        service.redis = mockRedis;
+        jest
+          .spyOn(mockRedis, 'lrange')
+          .mockImplementation(async () => ['mock refresh token']);
+
+        await service.isRefreshTokenValid('userId', 'mock refresh token');
+      });
+    });
+
+    describe('When refresh token is not present in db', () => {
+      it('should throw UnauthorizedException', async () => {
+        const mockRedis = new MockRedis();
+        // @ts-ignore
+        service.redis = mockRedis;
+        jest.spyOn(mockRedis, 'lrange').mockImplementation(async () => []);
+
+        try {
+          await service.isRefreshTokenValid('userId', 'mock refresh token');
+          expect('This line not to be executed').toBeFalsy();
+        } catch (e) {
+          expect(e).toBeInstanceOf(UnauthorizedException);
+          expect(e?.message).toBe('refresh token is not valid');
+        }
       });
     });
   });
