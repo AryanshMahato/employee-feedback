258390e35983d0f62af146ca5569b97a05e87faf
Aryansh Mahato <aryanshmahato@gmail.com>
2021-04-04 13:16:07 +0530

    [EF-5] (Add) get invitations controller tests

--- src/invitation/invitation.controller.spec.ts
+++ src/invitation/invitation.controller.spec.ts
@@ -19,12 +19,16 @@ import { TeamService } from '../team/team.service';
 import { UserService } from '../user/user.service';
 import { UserDocument } from '../user/user.schema';
 import { TeamDocument } from '../team/team.schema';
+import { AuthService } from '../auth/auth.service';
+import { JWTPayload } from '../auth/auth.types';
+import { TInvitation } from './invitation.types';
 
 describe('InvitationController', () => {
   let controller: InvitationController;
   let service: InvitationService;
   let teamService: TeamService;
   let userService: UserService;
+  let authService: AuthService;
 
   const invitationModel = new InvitationModel();
 
@@ -49,6 +53,7 @@ describe('InvitationController', () => {
     service = module.get<InvitationService>(InvitationService);
     userService = await module.get<UserService>(UserService);
     teamService = await module.get<TeamService>(TeamService);
+    authService = await module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
@@ -56,6 +61,7 @@ describe('InvitationController', () => {
     expect(service).toBeDefined();
     expect(userService).toBeDefined();
     expect(teamService).toBeDefined();
+    expect(authService).toBeDefined();
   });
 
   describe('sendInvitation', () => {
@@ -196,6 +202,49 @@ describe('InvitationController', () => {
         } catch (e) {
           expect(e).toBeInstanceOf(UnauthorizedException);
         }
+      });
+    });
+  });
+
+  describe('getInvitations()', () => {
+    const mockJwtPayload: JWTPayload = {
+      exp: 3600,
+      iat: 3600,
+      tokenType: 'Bearer',
+      userId: 'userId',
+    };
+
+    const mockInvitations = [
+      {
+        user: 'user',
+        accepted: false,
+        deleted: false,
+        rejected: false,
+        team: 'team',
+      },
+    ] as TInvitation[];
+
+    describe('When invitations are found', () => {
+      it('should return all invitations', async () => {
+        const requestMock = {
+          headers: {
+            authorization: 'Bearer token',
+          },
+        } as Request;
+
+        jest
+          .spyOn(authService, 'getUserFromToken')
+          .mockImplementation(() => mockJwtPayload);
+
+        jest
+          .spyOn(service, 'getInvitationsByUserId')
+          .mockImplementation(async () => mockInvitations);
+
+        const invitations = await controller.getInvitations(requestMock);
+
+        expect(invitations).toEqual({ invitations: mockInvitations });
+        expect(authService.getUserFromToken).toBeCalledTimes(1);
+        expect(service.getInvitationsByUserId).toBeCalledTimes(1);
       });
     });
   });
